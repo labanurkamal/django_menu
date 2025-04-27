822a30f01b90c36d36051827bf10067e19e6d870
Aibek <154282821+labanurkamal@users.noreply.github.com>
2025-04-28 01:04:36 +0500

    Add bash command run app.

---
+++ docker/backend/entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+echo "Контейнер стартует"
+
+echo "Применяем миграцию..."
+python manage.py makemigrations
+python manage.py migrate
+echo "Миграции успешно применены!"
+
+echo "Собираем static файлы"
+python manage.py collectstatic --no-input
+echo "Static файлы успешно созданы!"
+
+echo "Создание супер пользователя"
+python manage.py init_superuser
+echo "Супер пользователь успешно создан"
+
+echo "-------Запуск приложения-------"
+exec gunicorn --bind 0.0.0.0:8000 backend.wsgi
